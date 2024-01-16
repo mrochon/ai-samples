e251b3b80b627791e95db16ec7b6fd87101640d6
MariusR <mrochon@microsoft.com>
2024-01-15 20:38:31 -0800

    Upgraded to .NET 8 1.0.0 beta

--- console-qstarts/Program.cs
+++ console-qstarts/Program.cs
@@ -22,9 +22,6 @@ switch(type)
     case "chat1":
         await Chat1(openAI!.Endpoint, openAI.Key, openAI.Deployment);
         break;
-    case "chat2":
-        Chat2(openAI!.Endpoint, openAI.Key, openAI.Deployment);
-        break;
     case "nonchat":
         NonChat(openAI!.Endpoint, openAI.Key, openAI.Deployment);
         break;
@@ -52,7 +49,7 @@ async Task Ask(string endpoint, string key, string deploymentOrModelName)
     Console.Write($"Input: {prompt}\n");
 
     Response<Completions> completionsResponse =
-        await client.GetCompletionsAsync(engine, prompt);
+        await client.GetCompletionsAsync(new CompletionsOptions(deploymentOrModelName, new string[] {prompt }));
     string completion = completionsResponse.Value.Choices[0].Text;
     Console.WriteLine($"Chatbot: {completion}");
 }
@@ -62,79 +59,47 @@ async Task Chat1(string endpoint, string key, string deploymentOrModelName)
 {
     OpenAIClient client = new(new Uri(openAI.Endpoint), new AzureKeyCredential(openAI.Key));
 
-    var chatCompletionsOptions = new ChatCompletionsOptions()
-    {
-        Messages =
-    {
-        new ChatMessage(ChatRole.System, "You are a helpful assistant."),
-        new ChatMessage(ChatRole.User, "Does Azure OpenAI support customer managed openAI.Keys?"),
-        new ChatMessage(ChatRole.Assistant, "Yes, customer managed openAI.Keys are supported by Azure OpenAI."),
-        new ChatMessage(ChatRole.User, "Do other Azure AI services support this too?"),
-    },
-        MaxTokens = 100
-    };
-
-    Response<StreamingChatCompletions> response = await client.GetChatCompletionsStreamingAsync(
-        deploymentOrModelName: deploymentOrModelName,
-        chatCompletionsOptions);
-    using StreamingChatCompletions streamingChatCompletions = response.Value;
-
-    await foreach (StreamingChatChoice choice in streamingChatCompletions.GetChoicesStreaming())
-    {
-        await foreach (ChatMessage message in choice.GetMessageStreaming())
+    var chatCompletionsOptions = new ChatCompletionsOptions(deploymentOrModelName, new ChatRequestMessage[]
         {
-            Console.Write(message.Content);
-        }
-        Console.WriteLine();
-    }
-
-    Console.WriteLine();
-}
-
-void Chat2(string endpoint, string key, string deploymentOrModelName)
-{
-    OpenAIClient client = new(new Uri(openAI.Endpoint), new AzureKeyCredential(openAI.Key));
-
-    var response = client.GetChatCompletions(
-               deploymentOrModelName: deploymentOrModelName,
-                      new ChatCompletionsOptions()
-                      {
-            Messages =
-                          {
-                new ChatMessage(ChatRole.System, "You are a helpful assistant."),
-                new ChatMessage(ChatRole.User, "Does Azure OpenAI support customer managed openAI.Keys?"),
-                new ChatMessage(ChatRole.Assistant, "Yes, customer managed openAI.Keys are supported by Azure OpenAI."),
-                new ChatMessage(ChatRole.User, "Do other Azure AI services support this too?"),
-            },
-            MaxTokens = 100
+            new ChatRequestSystemMessage("You are a helpful assistant."),
+            new ChatRequestUserMessage("Does Azure OpenAI support customer managed openAI.Keys?"),
+            new ChatRequestSystemMessage("Yes, customer managed openAI.Keys are supported by Azure OpenAI."),
+            new ChatRequestUserMessage("Do other Azure AI services support this too?"),
         });
 
-    Console.WriteLine(response.Value.Choices[0].Message.Content);
+    using (StreamingResponse<StreamingChatCompletionsUpdate> response = await client.GetChatCompletionsStreamingAsync(chatCompletionsOptions))
+    {
+        var txt = new StringBuilder();
+        await foreach (var choice in response)
+        {
+            if (choice.ContentUpdate != null)
+            {
+                var txt1 = choice.ContentUpdate;
+                txt.Append(txt1);
+                // if (choice.Completion.Length > 0 && choice.Completion[^1] != ' ')
+                //     txt.Append(' ');
+            }
+        }
 
-    Console.WriteLine();
+        Console.WriteLine();
+    }
 }
 
 void NonChat(string endpoint, string key, string deploymentOrModelName)
 {
     OpenAIClient client = new(new Uri(openAI.Endpoint), new AzureKeyCredential(openAI.Key));
 
-    var response = client.GetChatCompletions(
-               deploymentOrModelName: deploymentOrModelName,
-                      new ChatCompletionsOptions()
-                      {
-                          Messages =
-                          {
-                            new ChatMessage(ChatRole.System, @"""You are an assistant designed to extract entities from text. Users will paste in a string of text and you will respond with entities you've extracted from the text as a JSON object. Here's an example of your output format:
-                            {
-                               ""name"": """",
-                               ""company"": """",
-                               ""phone_number"": """",
-                               ""subject"": """",
-                            }"""),
-                            new ChatMessage(ChatRole.User, "Hello. My name is Robert Smith. I'm calling from Contoso Insurance, Delaware. My colleague mentioned that you are interested in learning about our comprehensive benefits policy. Please call me back at (555) 346-9322. I would like to go over the benefits."),
-                          },
-                          MaxTokens = 100
-                      });
+    var response = client.GetChatCompletions(new ChatCompletionsOptions(deploymentOrModelName, new ChatRequestMessage[]
+        {
+            new ChatRequestSystemMessage(@"""You are an assistant designed to extract entities from text. Users will paste in a string of text and you will respond with entities you've extracted from the text as a JSON object. Here's an example of your output format:
+            {
+                ""name"": """",
+                ""company"": """",
+                ""phone_number"": """",
+                ""subject"": """",
+            }"""),
+            new ChatRequestUserMessage("Hello. My name is Robert Smith. I'm calling from Contoso Insurance, Delaware. My colleague mentioned that you are interested in learning about our comprehensive benefits policy. Please call me back at (555) 346-9322. I would like to go over the benefits."),
+        }));
 
     Console.WriteLine(response.Value.Choices[0].Message.Content);
 
@@ -145,14 +110,10 @@ async Task Conversation(string endpoint, string key, string deploymentOrModelNam
 {
     OpenAIClient client = new(new Uri(openAI.Endpoint), new AzureKeyCredential(openAI.Key));
 
-    var messages = new ChatCompletionsOptions()
+    var messages = new ChatCompletionsOptions(deploymentOrModelName, new ChatRequestMessage[]
     {
-        Messages =
-        {
-            new ChatMessage(ChatRole.System, "Users will ask you questions. Answer in Danish."),
-        },
-        MaxTokens = 100
-    };
+            new ChatRequestSystemMessage("Users will ask you questions. Answer in Danish."),
+    });
 
     do
     {
@@ -160,21 +121,17 @@ async Task Conversation(string endpoint, string key, string deploymentOrModelNam
         string? userMessage = Console.ReadLine();
         if (string.IsNullOrEmpty(userMessage))
             break;
-        messages.Messages.Add(new ChatMessage(ChatRole.User, userMessage));
-        Response<StreamingChatCompletions> response = await client.GetChatCompletionsStreamingAsync(
-            deploymentOrModelName: deploymentOrModelName,
-            messages);
-        using StreamingChatCompletions streamingChatCompletions = response.Value;
+        messages.Messages.Add(new ChatRequestUserMessage(userMessage));
+        StreamingResponse<StreamingChatCompletionsUpdate> response = await client.GetChatCompletionsStreamingAsync(messages);
 
-        await foreach (StreamingChatChoice choice in streamingChatCompletions.GetChoicesStreaming())
+        var streamingChatCompletions = response.GetRawResponse();
+
+        await foreach (var choice in response)
         {
-            await foreach (ChatMessage message in choice.GetMessageStreaming())
-            {
-                Console.Write(message.Content);
-            }
+            Console.Write(choice.ContentUpdate);
             Console.WriteLine();
         }
-        messages.Messages.Add(new ChatMessage(ChatRole.System, "Now start answering in English"));
+        messages.Messages.Add(new ChatRequestAssistantMessage("Now start answering in English"));
         Console.WriteLine();
     } while (true);
 
@@ -201,17 +158,15 @@ async Task ConversationWithFunctions(string endpoint, string key, string deploym
             })
         }
     };
-    var messages = new ChatCompletionsOptions()
+    var messages = new ChatCompletionsOptions(deploymentOrModelName, new ChatRequestMessage[]
     {
-        Messages =
-        {
-            new ChatMessage(ChatRole.System, "Please use functions whenever possible."),
-        },
+        new ChatRequestSystemMessage("Please use functions whenever possible."),
+    })
+    { 
         Functions = functions,
         MaxTokens = 100
     };
-
-
+ 
     var msgAdded = false;
     do
     {
@@ -221,66 +176,43 @@ async Task ConversationWithFunctions(string endpoint, string key, string deploym
             string? userMessage = Console.ReadLine();
             if (string.IsNullOrEmpty(userMessage))
                 break;
-            messages.Messages.Add(new ChatMessage(ChatRole.User, userMessage));
+            messages.Messages.Add(new ChatRequestUserMessage(userMessage));
         }
         msgAdded = false;
-        Response<StreamingChatCompletions> response = await client.GetChatCompletionsStreamingAsync(
-            deploymentOrModelName: deploymentOrModelName,
-            messages
-            );
-        using StreamingChatCompletions streamingChatCompletions = response.Value;
-
+        StreamingResponse<StreamingChatCompletionsUpdate> response = await client.GetChatCompletionsStreamingAsync(messages);
         //TDO: Handle multiple function call requests in one response
         string funcName = String.Empty;
         var argumentsJson= new StringBuilder();
-        await foreach (StreamingChatChoice choice in streamingChatCompletions.GetChoicesStreaming())
+        await foreach (var choice in response)
         {
-            await foreach (ChatMessage message in choice.GetMessageStreaming())
-            {
-                var function = message.FunctionCall;
-                if (function != null)
-                {
-                    if (!string.IsNullOrEmpty(function.Name))
-                    {
-                        funcName = function.Name;
-                    }
-                    if (!string.IsNullOrEmpty(function.Arguments))
-                    {
-                        argumentsJson.Append(function.Arguments);
-                    }
-                }
-                else
-                {
-                    Console.Write(message.Content);
-                }
-            }
+            Console.Write($"Response type: {choice.Role}");
         }
         // If function call(s) was(were) returned above, call it and add response as a message
-        if (!string.IsNullOrEmpty(funcName))
-        {
-            Dictionary<string, string> args;
-            ParseFunctiondefinition(argumentsJson.ToString(), out args);
-            switch(funcName)
-            {                     
-                case "getHotels":
-                    messages.Messages.Add(new ChatMessage() 
-                    {
-                        Role = ChatRole.Assistant,
-                        FunctionCall = new FunctionCall(funcName, argumentsJson.ToString()),
-                    });
-                    messages.Messages.Add(new ChatMessage() {
-                        Role = ChatRole.Function,
-                        Name = funcName,
-                        Content = GetHotels(args["location"])
-                    });
-                    msgAdded = true;
-                    break;
-                default:
-                    messages.Messages.Add(new ChatMessage(ChatRole.Assistant, $"Function {funcName} not implemented"));
-                    break;
-            }    
-            funcName = String.Empty;
-        }
+        //if (!string.IsNullOrEmpty(funcName))
+        //{
+        //    Dictionary<string, string> args;
+        //    ParseFunctiondefinition(argumentsJson.ToString(), out args);
+        //    switch(funcName)
+        //    {                     
+        //        case "getHotels":
+        //            messages.Messages.Add(new ChatRequestMessage() 
+        //            {
+        //                Role = ChatRole.Assistant,
+        //                FunctionCall = new FunctionCall(funcName, argumentsJson.ToString()),
+        //            });
+        //            messages.Messages.Add(new ChatRequestUserMessage() {
+        //                Role = ChatRole.Function,
+        //                Name = funcName,
+        //                Content = GetHotels(args["location"])
+        //            });
+        //            msgAdded = true;
+        //            break;
+        //        default:
+        //            messages.Messages.Add(new ChatRequestAssistantMessage(ChatRole.Assistant, $"Function {funcName} not implemented"));
+        //            break;
+        //    }    
+        //    funcName = String.Empty;
+        //}
         Console.WriteLine();
     } while (true);
 
@@ -329,6 +261,7 @@ void ParseFunctiondefinition(string json, out Dictionary<string,string> args)
                 // Other token types elided for brevity
         }
     }
+    
 }
 
 public class FunctionArgument
