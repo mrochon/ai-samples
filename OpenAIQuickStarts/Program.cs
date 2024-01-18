2a3d1b4eb725729c2d44a978eb99adfe5597bf32
MariusR <mrochon@microsoft.com>
2024-01-17 19:48:14 -0800

    Removed functions, obsolete

--- OpenAIQuickStarts/Program.cs
+++ OpenAIQuickStarts/Program.cs
@@ -30,9 +30,6 @@ switch(type)
     case "conversation":
         await Conversation(openAI!.Endpoint, openAI.Key, openAI.Deployment);
         break;
-    case "conversationwithfunctions":
-        await ConversationWithFunctions(openAI!.Endpoint, openAI.Key, openAI.Deployment);
-        break;
     case "chatWithTools":
         await ChatWithTools.Run(openAI!);
         break;
@@ -143,104 +140,4 @@ async Task Conversation(string endpoint, string key, string deploymentOrModelNam
     Console.WriteLine();
 }
 
-async Task ConversationWithFunctions(string endpoint, string key, string deploymentOrModelName)
-{
-    OpenAIClient client = new(new Uri(openAI.Endpoint), new AzureKeyCredential(openAI.Key));
 
-
-    var getWeatherTool = new ChatCompletionsFunctionToolDefinition()
-    {
-        Name = "get_current_weather",
-        Description = "Get the current weather in a given location",
-        Parameters = BinaryData.FromObjectAsJson(
-    new
-    {
-        Type = "object",
-        Properties = new
-        {
-            Location = new
-            {
-                Type = "string",
-                Description = "The city and state, e.g. San Francisco, CA",
-            },
-            Unit = new
-            {
-                Type = "string",
-                Enum = new[] { "celsius", "fahrenheit" },
-            }
-        },
-        Required = new[] { "location" },
-    },
-    new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase }),
-    };
-    var functions = new FunctionDefinition[]
-    {
-        new FunctionDefinition()
-        {
-            Name = "getHotels",
-            Description = "Retrieves hotels from the search index based on the parameters provided",
-            Parameters = BinaryData.FromObjectAsJson(new
-            {
-                type = "object",
-                required = new[] { "location" },
-                properties =  new {
-                    location = new { type = "string", description = "The location of the hotel (i.e. Seattle, WA)" } ,
-                    max_price = new { type = "number", description = "The maximum price for the hotel" } ,
-                    features = new { type = "string", description = "A comma separated list of features (i.e. beachfront, free wifi, etc.)" } 
-                }
-            })
-        }
-    };
-    var options = new ChatCompletionsOptions(deploymentOrModelName, new ChatRequestMessage[]
-    {
-        new ChatRequestSystemMessage("Please use functions whenever possible."),
-    })
-    { 
-        ChoiceCount = 1,
-        Functions = functions,
-        MaxTokens = 100,
-        ResponseFormat = ChatCompletionsResponseFormat.Text,
-    };
- 
-    var msgAdded = false;
-    do
-    {
-        if (!msgAdded)
-        {
-            Console.Write("User: ");
-            string? userMessage = Console.ReadLine();
-            if (string.IsNullOrEmpty(userMessage))
-                break;
-            options.Messages.Add(new ChatRequestUserMessage(userMessage));
-        }
-        msgAdded = false;
-        var response = await client.GetChatCompletionsAsync(options);
-        foreach(var choice in response.Value.Choices)
-        {
-            Console.WriteLine($"[{choice.Message.Role}]: {choice.Message.Content}");
-            if (choice.FinishReason == CompletionsFinishReason.FunctionCall)
-            {
-                Console.WriteLine($"{choice.Message.FunctionCall.Name}({choice.Message.FunctionCall.Arguments})");
-                switch (choice.Message.FunctionCall.Name)
-                {
-                    case "getHotels":
-                        var p = choice.Message.FunctionCall.Arguments;
-                        options.Messages.Add(new ChatRequestToolMessage(GetHotels(p), "x"));
-                        msgAdded = true;
-                        break;
-                    default:
-                        Console.WriteLine("Function not found");
-                        break;
-                }
-            }
-        }
-        Console.WriteLine();
-    } while (true);
-
-    Console.WriteLine();
-}
-
-string GetHotels(string parameters)
-{
-    return $"Hotels in Seattle: Marriott, Hyatt, Motel 6";
-}
